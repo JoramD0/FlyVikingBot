11df6759c27a63004a0973106dffac550501ce95
JoramD0 <j.davids@hotmail.nl>
2024-06-13 16:24:15 +0200

    Fix crash with `rssParser` in case website is down

--- rssParser.py
+++ rssParser.py
@@ -5,10 +5,16 @@ async def read_feed(rss_url, pretty_name, callback):
     if os.path.exists(f"{pretty_name}_rss_trackfile"):
         with open(f"{pretty_name}_rss_trackfile", "r") as trackfile:
             original_trackfile_time = trackfile.read()
-            feed = feedparser.parse(rss_url, modified=original_trackfile_time)
+            try:
+                feed = feedparser.parse(rss_url, modified=original_trackfile_time)
+            else:
+                logging.error("rssParser: failed to read feed")
     else:
         original_trackfile_time = time.strftime("%a, %d %b %Y %H:%M:%S %z", time.gmtime()) # To prevent error on first run
-        feed = feedparser.parse(rss_url)
+        try:
+            feed = feedparser.parse(rss_url)
+        else:
+            logging.error("rssParser: failed to read feed")
 
     # Write latest modified if new feed
     if feed.status == 200:
